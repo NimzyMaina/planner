0b7d015ee87c7fab4cc4220f91edb570ba1dd724
NimzyMaina <nimzy.maina@gmail.com>
2016-01-13 11:06:20 +0300

    exec

--- test.php
+++ test.php
@@ -1,13 +1,18 @@
 <?php
 
-		$host = "localhost";
-		$db_name = "planner";
-		$username = "root";
-		$password = "";
+		// $host = "localhost";
+		// $db_name = "planner";
+		// $username = "root";
+		// $password = "";
  
-        try{
-            $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
-            echo "connection established";
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
-        }
+  //       try{
+  //           $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+  //           echo "connection established";
+  //       }catch(PDOException $exception){
+  //           echo "Connection error: " . $exception->getMessage();
+  //       }
+
+    exec("git remote -v", $output, $return);
+    echo "<pre>";
+    echo "Dir returned $return, and output:\n";
+    var_dump($output);
